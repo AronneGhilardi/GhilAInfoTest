f98ba4c271ea5c184cadc4edf27339ad8371b584
Aronne Ghilardi <aronneghilardi05@gmail.com>
2023-03-27 10:58:20 +0200

    Task5

--- GhilA_InfoTest/Form1.cs
+++ GhilA_InfoTest/Form1.cs
@@ -16,5 +16,62 @@ namespace GhilA_InfoTest
         {
             InitializeComponent();
         }
+
+        List<GhilA_Candidato> lista = new List<GhilA_Candidato>();
+
+        public void Aggiungere(GhilA_Candidato y)
+        {
+            lista.Add(y);
+        }
+
+        public string[] Visualizza(List<GhilA_Candidato> y)
+        {
+            string[] s = new string[999];
+            int i = 0;
+            foreach (GhilA_Candidato x in lista)
+            {
+                s[i] = x.ToString();
+                i++;
+            }
+            return s;
+        }
+
+        public void Modificare(string nome, int NrMatricola)
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i].Matricola == NrMatricola)
+                {
+                    lista[i].Nome = nome;
+                }
+            }
+        }
+
+        public void Eliminare(int NrMatricola)
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i].Matricola == NrMatricola)
+                {
+                    lista[i] = null;
+                }
+            }
+        }
+
+        public void VisualizzareIdonei()
+        {
+            string[] s = new string[999];
+            int i = 0;
+            foreach (GhilA_Candidato x in lista)
+            {
+                bool b = x.isIdoneo();
+                if (b == true)
+                {
+                    s[i] = x.ToString();
+                    i++;
+                }
+            }
+            return s;
+        }
     }
 }
